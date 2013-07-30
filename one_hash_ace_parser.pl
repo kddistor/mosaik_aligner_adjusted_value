7d94e63010ab42f9575c07451ef47dc4c8146e0e
Kevin Distor <kddistor@ucdavis.edu>
2013-07-30 11:29:21 -0700

    Update one_hash_ace_parser.pl
    Added comments

--- one_hash_ace_parser.pl
+++ one_hash_ace_parser.pl
@@ -1,19 +1,35 @@
 #!/usr/bin/perl
-#One hash Mosaik ace file parser
-#by Kevin Distor
 
+
+#####################################
+#	One Hash Mosaik Ace File Parser	#
+#	by Kevin Distor					#
+#	University of California, Davis	#
+#############################################################################################################################
+#STEP 2 - Align TE from alignment file to reference TE file.																#
+#																															#
+#	The assembly file format is then run through a perl script one_hash_ace_parser.pl which takes in a reference 			#
+#	Transposable Element(TE) file(in fasta format)and counts once if a TE appears in an ace file to show the number 		#
+#	of times a TE uniquely maps to a SimSeqRead. It produces a grid with the reads that the script obtained from 			#
+#	the ace files in the first column and the TEs that that script obtained from the TE reference file in the first row.	# 
+#	Reads that aligned to a TE will have a 1 marked in the crossposition. The output is labeled "parsed_ace_final.csv" by 	#
+#	default.																												#
+#																															#
+#############################################################################################################################
+
+#Declare all global variables
 my ($teCount, $myTE, $file, $i, $hash);
 my (@teArray, @array, @files);
 my (%hash);
 
-#grab all files ending with ace in the current directory
+#Grab all files ending with ace in the current directory
 @files = glob("*.ace");
 open(FINAL, '>parsed_ace_final.csv');
 
-#grab all reads and store to an array
+#Grab all TEs from reference and store to an array
 open(FASTA, '<UTE_tags.fasta');
 while (<FASTA>) {
-  chomp;
+	chomp;
 	if ($_ =~ m/\>(\S+)/) {
 		chomp;
 		push (@teArray, "$1,");
@@ -24,16 +40,25 @@ while (<FASTA>) {
 foreach $file (@files) {
 	open(FILE, $file);
 	while (<FILE>) {
-		#if file is RITD0001.2.fastq.dat.UTE_tags.assembled_[RLX_osed_AC191084-2931.1] grab what's in brackets
-		$file =~ s/\D{4}\d{4}\.\d{1}\.\D{5}\.\D{3}\.\D{3}\_\D{4}\.\D{9}\_//g;
-		$file =~ s/\D{4}\d{4}\.\d{2}\.\D{5}\.\D{3}\.\D{3}\_\D{4}\.\D{9}\_//g;
-		#if file is RIMMA0001.2.fastq.dat.UTE_tags.assembled_[RLX_osed_AC191084-2931.1] grab what's in brackets
+		#Get the TEs from sequence file to be matched against the reference TE array. 
+		#Examples precede the code. This part grabs what is denoted by the bracket.
+		#RIMMA0385.1.fastq.dat.UTE_tags.assembled_[RST_ZmSINE3_consensus-0.1].ace
 		$file =~ s/\D{5}\d{4}\.\d{1}\.\D{5}\.\D{3}\.\D{3}\_\D{4}\.\D{9}\_//g;
+		#RIMMA0030.13.fastq.dat.UTE_tags.assembled_[RST_ZmSINE2_consensus-0.1].ace
 		$file =~ s/\D{5}\d{4}\.\d{2}\.\D{5}\.\D{3}\.\D{3}\_\D{4}\.\D{9}\_//g;
+		#if file is RITD0001.2.fastq.dat.UTE_tags.assembled_[RLX_osed_AC191084-2931.1].ace 
+		$file =~ s/\D{4}\d{4}\.\d{1}\.\D{5}\.\D{3}\.\D{3}\_\D{4}\.\D{9}\_//g;
+		#if file is RITD0001.01.fastq.dat.UTE_tags.assembled_[DTA_ZM00346_consensus.1].ace
+		$file =~ s/\D{4}\d{4}\.\d{2}\.\D{5}\.\D{3}\.\D{3}\_\D{4}\.\D{9}\_//g;
 		$file =~ s/\.ace/,/;
+		print "$file\n";
 		chomp;
+		#Get read names from each sequnce file and if there is a match of TEs from sequence file to that of reference TE array. 
+		#Denote a 1 if there is a match, else put NULL.
 		if ($_=~ m/^AF\s(\D+\-\D+\d+\_\d+\:\d\:\d+\:\d+\:\d+)/) {
-			#AF HWI-ST611_0210:8:1101:7570:2203#0/1 U 2982
+			#The read matching statement for this part is for UTE_tags reference.
+			#The format of reads for UTE_tags reference is: AF HWI-ST611_0210:8:1101:7570:2203#0/1 U 2982
+			#If using a different reference than UTE_tags, modify previous line of code.
 				my ($key)=$1;
 				my($index)= grep { $teArray[$_] eq "$file" } 0..$teCount;
 				$hash{$key}[$index] = 1;
@@ -41,6 +66,8 @@ foreach $file (@files) {
 	}
 }
 
+#Print the output. Default output is parsed_ace_final.csv. Change line 18 to change ouput name. Only output names
+#ending with .csv will be used for the rest of the pipeline.
 print FINAL "SimSeqReadName, @teArray\n";
 foreach (sort keys %hash) {
 	print FINAL "$_,";
